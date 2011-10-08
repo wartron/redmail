8f2a9129e5569dff242c23fd3daffc64fe8f1665
willwharton <will@wideopentech.com>
2011-10-08 13:15:37 -0400

    reduced size

--- redmail.py
+++ redmail.py
@@ -8,44 +8,38 @@ import sys
 import pywinusb.hid as hid
 import reddit
 
-def get_device(target_vendor_id):
-	all_devices = hid.HidDeviceFilter(vendor_id = target_vendor_id).get_devices()
-	
-	if not all_devices:
-		print "Can't find target device (vendor_id = 0x%04x)!" % target_vendor_id
+def get_device():
+	all_devices = hid.HidDeviceFilter(vendor_id = 0x1294).get_devices()
+		if len(all_devices)==0:
+		print "Can't find target device"		
 	else:		
 		return all_devices[0]
-		
 	return null
 	
-def set_color(device,color,target_usage):
-		for report in device.find_output_reports():
-			if target_usage in report:
-				report[target_usage] =  [color,0,0,0,0] 
-				report.send() 				
-
-				
-				
-if __name__ == '__main__':
-	target_vendor_id = 0x1294
-	target_usage = hid.get_full_usage_id(0xff00, 0x0001)
 	
+def set_color(device,color):
+		for report in device.find_output_reports():
+			if 4278190081 in report:
+				report[4278190081] =  [color,0,0,0,0] 
+				report.send() 				
+	
+				
+if __name__ == '__main__':	
 	try:
-		d = get_device(target_vendor_id)
+		d = get_device()
 		d.open()
 		
-		set_color(d,0,target_usage)
+		set_color(d,0)
 		r = reddit.Reddit(user_agent="redmail")
 		r.login(user=user,password=password)
 		inbox = r.get_inbox()
 		
-		while True:
-			
+		while True:			
 			new = inbox.get_new_messages(force=True)
 			if len(new) > 0:
-				set_color(d,2,target_usage)
+				set_color(d,2)
 			else:
-				set_color(d,0,target_usage)				
+				set_color(d,0)				
 				
 			time.sleep(delay)
 		
@@ -53,6 +47,3 @@ if __name__ == '__main__':
 		d.close()
 
 	exit()
-
-
-
